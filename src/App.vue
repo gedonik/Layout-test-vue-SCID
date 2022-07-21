4580011b70c5344ec997e0a81d6c9ee98dae392d
gedonik <gedonik@bk.ru>
2022-07-21 12:29:37 +0300

    начало проекта

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import AppHeader from "@/components/AppHeader";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {AppHeader},
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 0 15px;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<header class="header">
+
+</header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
