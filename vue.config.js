0ff4fedc5a722aa96c556ba5802ce52a8cb3aef2
gedonik <gedonik@bk.ru>
2022-07-22 11:53:31 +0300

    Добавил билд в docs-5

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/Layout-test-vue-SCID/'
-      : '/'
+      : './'
   // outputDir: path.resolve(__dirname, "docs"),
   // publicPath: './',
   // assetsDir: "../../static/SPA"
